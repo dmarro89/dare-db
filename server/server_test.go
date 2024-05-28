b888ff34148198a9cd8b0046224f0ca674d39f0e
Davide Marro <davideraffaele.marro@gmail.com>
2024-05-28 17:55:20 +0200

    Revert "Adding https server implementation (#10)"
    This reverts commit ca50fa6f6dbaefd09841d423bf9a0fa314a66482.

--- server/server_test.go
+++ server/server_test.go
@@ -1,106 +1,108 @@
 package server
 
 import (
+	"bytes"
+	"encoding/json"
 	"net/http"
-	"os"
+	"net/http/httptest"
 	"testing"
-	"time"
 
-	"github.com/stretchr/testify/mock"
+	"github.com/dmarro89/dare-db/database"
 	"gotest.tools/assert"
 )
 
-// constants
-const SLEEP_ON_START int = 3
+func TestServer_SetAndGet(t *testing.T) {
+	db := database.NewDatabase()
+	srv := NewServer(db)
 
-// Mock Database
-type MockDatabase struct {
-	mock.Mock
+	setWrongResponse := httptest.NewRecorder()
+	setWrongRequest, _ := http.NewRequest("GET", "/set", bytes.NewBuffer([]byte{}))
+
+	srv.HandlerSet(setWrongResponse, setWrongRequest)
+	assert.Equal(t, http.StatusMethodNotAllowed, setWrongResponse.Code, "Method not allowed")
+
+	setWrongFormatRequest, _ := http.NewRequest("POST", "/set", bytes.NewBuffer([]byte("plainText")))
+	setWrongFormatResponse := httptest.NewRecorder()
+	srv.HandlerSet(setWrongFormatResponse, setWrongFormatRequest)
+	assert.Equal(t, http.StatusBadRequest, setWrongFormatResponse.Code, "Invalid JSON format, the body must be in the form of {\"key\": \"value\"}")
+
+	setData := map[string]interface{}{"testKey": "testValue"}
+	setDataJSON, _ := json.Marshal(setData)
+	setRequest, _ := http.NewRequest("POST", "/set", bytes.NewBuffer(setDataJSON))
+
+	setResponse := httptest.NewRecorder()
+	srv.HandlerSet(setResponse, setRequest)
+	assert.Equal(t, http.StatusCreated, setResponse.Code, "Expected status %d, got %d", http.StatusCreated, setResponse.Code)
+
+	getEmptyRequest, _ := http.NewRequest("GET", "/get", nil)
+	getEmptyResponse := httptest.NewRecorder()
+	srv.HandlerGet(getEmptyResponse, getEmptyRequest)
+	assert.Equal(t, http.StatusBadRequest, getEmptyResponse.Code, `url param query "key" cannot be empty`)
+
+	getMissingKeyRequest, _ := http.NewRequest("GET", "/get/missingKey", nil)
+	getMissingKeyRequest.SetPathValue("key", "missingKey")
+	getMissingKeyResponse := httptest.NewRecorder()
+	srv.HandlerGet(getMissingKeyResponse, getMissingKeyRequest)
+	assert.Equal(t, http.StatusNotFound, getMissingKeyResponse.Code, `Key "%s" not found`, "missingKey")
+
+	getRequest, _ := http.NewRequest("GET", "/get/testKey", nil)
+	getRequest.SetPathValue("key", "testKey")
+	getResponse := httptest.NewRecorder()
+
+	srv.HandlerGet(getResponse, getRequest)
+
+	assert.Equal(t, http.StatusOK, getResponse.Code, "Expected status %d, got %d", http.StatusOK, getResponse.Code)
+
+	var getResult map[string]interface{}
+	err := json.Unmarshal(getResponse.Body.Bytes(), &getResult)
+	assert.NilError(t, err, "Error decoding JSON response")
+
+	for key, value := range getResult {
+		assert.Equal(t, key, "testKey", "Unexpected response body: %v", getResult)
+		assert.Equal(t, value, "testValue", "Unexpected response body: %v", getResult)
+	}
 }
 
-// Mock DareServer
-type MockDareServer struct {
-	mock.Mock
-}
+func TestServer_SetAndDelete(t *testing.T) {
+	db := database.NewDatabase()
+	srv := NewServer(db)
 
-func (ds *MockDareServer) CreateMux() *http.ServeMux {
-	args := ds.Called()
-	return args.Get(0).(*http.ServeMux)
-}
+	setData := map[string]interface{}{"testKey": "testValue"}
+	setDataJSON, _ := json.Marshal(setData)
+	setRequest, _ := http.NewRequest("POST", "/set", bytes.NewBuffer(setDataJSON))
+	setResponse := httptest.NewRecorder()
 
-func (ds *MockDareServer) HandlerGetById(w http.ResponseWriter, r *http.Request) {
-}
-func (ds *MockDareServer) HandlerSet(w http.ResponseWriter, r *http.Request) {
-}
-func (ds *MockDareServer) HandlerDelete(w http.ResponseWriter, r *http.Request) {
-}
+	srv.HandlerSet(setResponse, setRequest)
 
-func TestNewHttpServer(t *testing.T) {
-	server := NewHttpServer(&MockDareServer{})
-	assert.Assert(t, server != nil)
-	assert.Assert(t, server.configuration != nil)
-	assert.Assert(t, server.sigChan != nil)
-	assert.Assert(t, server.dareServer != nil)
-}
+	assert.Equal(t, http.StatusCreated, setResponse.Code, "Expected status %d, got %d", http.StatusCreated, setResponse.Code)
 
-func TestHttpServerStartAndStop(t *testing.T) {
-	// Setup
-	sigChan := make(chan os.Signal, 1)
-	server := &HttpServer{
-		configuration: NewConfiguration(),
-		sigChan:       sigChan,
-		dareServer:    &MockDareServer{},
+	deleteWrongResponse := httptest.NewRecorder()
+	deleteWrongRequest, _ := http.NewRequest("GET", "/delete", nil)
+	srv.HandlerDelete(deleteWrongResponse, deleteWrongRequest)
+	assert.Equal(t, http.StatusMethodNotAllowed, deleteWrongResponse.Code, "Method not allowed")
+
+	deleteEmptyRequest, _ := http.NewRequest("DELETE", "/delete", nil)
+	deleteEmptyResponse := httptest.NewRecorder()
+	srv.HandlerDelete(deleteEmptyResponse, deleteEmptyRequest)
+	assert.Equal(t, http.StatusBadRequest, deleteEmptyResponse.Code, `url param query "key" cannot be empty`)
+
+	deleteRequest, _ := http.NewRequest("DELETE", "/delete/testKey", nil)
+	deleteRequest.SetPathValue("key", "testKey")
+	deleteResponse := httptest.NewRecorder()
+
+	srv.HandlerDelete(deleteResponse, deleteRequest)
+
+	if deleteResponse.Code != http.StatusOK {
+		t.Errorf("Expected status %d, got %d", http.StatusNoContent, deleteResponse.Code)
 	}
 
-	mux := http.NewServeMux()
-	server.dareServer.(*MockDareServer).On("CreateMux").Return(mux)
+	getRequest, _ := http.NewRequest("GET", "/get/testKey", nil)
+	getRequest.SetPathValue("key", "testKey")
+	getResponse := httptest.NewRecorder()
 
-	// Start server
-	go server.Start()
-	time.Sleep(time.Duration(SLEEP_ON_START) * time.Second) // Give it time to start
+	srv.HandlerGet(getResponse, getRequest)
 
-	// Verify the server is running
-	assert.Assert(t, server.httpServer != nil)
-
-	// Stop server
-	server.Stop()
-
-	time.Sleep(time.Duration(SLEEP_ON_START) * time.Second)
-	// Verify the server is stopped
-	assert.Assert(t, server.httpServer == nil)
-}
-
-func TestNewHttpsServer(t *testing.T) {
-	server := NewHttpsServer(&MockDareServer{})
-	assert.Assert(t, server != nil)
-	assert.Assert(t, server.configuration != nil)
-	assert.Assert(t, server.sigChan != nil)
-	assert.Assert(t, server.dareServer != nil)
-}
-
-func TestHttpsServerStartAndStop(t *testing.T) {
-	t.Skip("Skipping test - Configure certificates to run it")
-	// Setup
-	sigChan := make(chan os.Signal, 1)
-	server := &HttpsServer{
-		configuration: NewConfiguration(),
-		sigChan:       sigChan,
-		dareServer:    &MockDareServer{},
+	if getResponse.Code != http.StatusNotFound {
+		t.Errorf("Expected status %d, got %d", http.StatusNotFound, getResponse.Code)
 	}
-
-	mux := http.NewServeMux()
-	server.dareServer.(*MockDareServer).On("CreateMux").Return(mux)
-
-	// Start server
-	go server.Start()
-	time.Sleep(time.Duration(SLEEP_ON_START) * time.Second) // Give it time to start
-
-	// Verify the server is running
-	assert.Assert(t, server.httpsServer != nil)
-
-	// Stop server
-	server.Stop()
-
-	// Verify the server is stopped
-	assert.Assert(t, server.httpsServer == nil)
 }
