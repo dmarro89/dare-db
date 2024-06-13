4457529397a0dea3425cf0ce1bbc22cfa6668366
go-while <go-while@users.noreply.github.com>
2024-06-13 09:48:22 +0200

    Update constants.go

--- server/constants.go
+++ server/constants.go
@@ -26,17 +26,52 @@ const DEFAULT_SERVER_SOCKET_TLS_PORT = "4420"
 const DEFAULT_TLS_PRIVKEY = "privkey.pem"
 const DEFAULT_TLS_PUBCERT = "fullchain.pem"
 
-const EmptyStr = ""
+// readline flags
+const no_mode = 0x00
+const modeADD = 0x11
+const modeGET = 0x22
+const modeSET = 0x33
+const modeDEL = 0x44
 const CaseAdded = 0x69
 const CaseDupes = 0xB8
 const CaseDeleted = 0x00
 const CasePass = 0xFF
 const FlagSearch = 0x42
 
-const no_mode = 0x00
-const modeADD = 0x11
-const modeGET = 0x22
-const modeSET = 0x33
-const modeDEL = 0x44
+// client proto flags
+const magic1 = "1" // mem-prof
+const magic2 = "2" // cpu-prof
+const magicA = "A" // add
+const magicD = "D" // del
+const magicG = "G" // get
+const magicL = "L" // list
+const magicS = "S" // set
+const magicZ = "Z" // quit
+
+// socket proto flags
+const EmptyStr = ""
+const CR = "\r"
+const LF = "\n"
+const CRLF = CR + LF
+const DOT = "."
+const COM = ","
+const SEM = ";"
+
+// ASCII control characters
+// [hex: 0 - 1F] // [DEC character code 0-31]
+const NUL = string(0x00) // Null character 		// 0
+const SOH = string(0x01) // Start of Heading 	// 1
+const STX = string(0x02) // Start of Text 		// 2
+const ETX = string(0x03) // End of Text 		// 3
+const EOT = string(0x04) // End of Transmission // 4
+const ENQ = string(0x05) // Enquiry 			// 5
+const ACK = string(0x06) // Acknowledge 		// 6
+const BEL = string(0x07) // Bell, Alert 		// 7
+const SYN = string(0x16) // Synchronous Idle	// 22
+const ETB = string(0x17) // End of Trans. Block // 23
+const CAN = string(0x18) // Cancel 				// 24
+const EOM = string(0x19) // End of medium 		// 25
+const SUB = string(0x20) // Substitute  		// 26
+const ESC = string(0x1B) // Escape 				// 27
 
 var Prof *prof.Profiler
