e2c8ebcf0298e774b1599b0d7fca738d0ae9be5b
Viktor Dmitriyev <2291074+vdmitriyev@users.noreply.github.com>
2025-03-25 18:43:32 +0100

    FIX: remove unused code

--- main.go
+++ main.go
@@ -1,25 +1,11 @@
 package main
 
 import (
-	"net/http"
-
 	"github.com/dmarro89/dare-db/auth"
 	"github.com/dmarro89/dare-db/database"
 	"github.com/dmarro89/dare-db/logger"
 	"github.com/dmarro89/dare-db/server"
-	"github.com/rs/cors"
 )
-
-// ApplyCORS applies CORS to the provided handler.
-func ApplyCORS(handler http.Handler, allowedOrigins []string) http.Handler {
-	c := cors.New(cors.Options{
-		AllowedOrigins:   allowedOrigins,
-		AllowCredentials: true,
-		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
-		AllowedHeaders:   []string{"Content-Type", "Authorization"},
-	})
-	return c.Handler(handler)
-}
 
 func main() {
 	logger := logger.NewDareLogger()
